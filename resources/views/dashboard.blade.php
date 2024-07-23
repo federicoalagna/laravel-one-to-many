cc8e1050b76055d9ccee10df53d1850e15abcd6c
federicoalagna <federicoalagna01@gmail.com>
2024-07-23 17:55:49 +0200

    [UPD] miglioramento dei colori delle varie sezioni

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.backoffice')
 
 @section('content')
-    <h1>Benvenuto nella Dashboard</h1>
-    <a href="{{ route('projects.index') }}">Gestione Progetti</a>
+
+    <body class="bg-light">
+        <h1 class="p-3">Benvenuto nella Dashboard</h1>
+        <a class="btn btn-primary p-3 m-3" href="{{ route('projects.index') }}">Gestione Progetti</a>
+    </body>
 @endsection
